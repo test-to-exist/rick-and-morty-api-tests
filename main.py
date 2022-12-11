7320576751b6428823b6404f662a48cf7dfabc1a
Test To Exist <test-to-exist@proton.me>
2022-12-11 19:19:58 +0100

    Add minor comment for the resources

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 
 def test_character_endpoint(name):
+
+    # TRY using this one:
+    # https: // realpython.com / python - testing /  # testing-for-web-frameworks-like-django-and-flask
+
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     baseUrl = 'rickandmortyapi.com/api';
     character_endpoint = f'https://{baseUrl}/character'

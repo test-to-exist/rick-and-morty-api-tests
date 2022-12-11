3882d8ba2585592eb823d5bb01b263b63dda9c24
Test To Exist <test-to-exist@proton.me>
2022-12-11 19:13:02 +0100

    initial commmit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+def test_character_endpoint(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    baseUrl = 'rickandmortyapi.com/api';
+    character_endpoint = f'https://{baseUrl}/character'
+    response = requests.get(character_endpoint)
+    print(response.json())
+
+if __name__ == '__main__':
+    test_character_endpoint('PyCharm')

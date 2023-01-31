427c8d084e25fdd78bf81e644623cbd7f12deb9c
Test To Exist <test-to-exist@proton.me>
2023-01-31 21:37:54 +0100

    Rearrange tests and change conditions a bit

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ class TestCharacterEndpoint(unittest.TestCase):
     def setUp(self):
         self.base_url = 'https://rickandmortyapi.com/api'
 
+    # POSITIVE character endpoint tests
     def test_character_options(self):
         response = requests.options(f"{self.base_url}/character")
         print(response.headers)
@@ -109,28 +110,36 @@ class TestCharacterEndpoint(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         validate(response.json(), schema=schema)
 
-    def test_character_should_return_error_for_an_invalid_param(self):
-        response = requests.get(f"{self.base_url}/character/0")
-        print(response.json())
-        response = requests.get(f"{self.base_url}/character/a")
-        print(response.json())
-
     def test_character_should_return_specific_list_for_multiple_param(self):
         response = requests.get(f"{self.base_url}/character/1,3,5")
         self.assertEqual(response.status_code, 200)
         response_body = response.json()
         self.assertEqual(len(response_body), 3)
 
-    def test_character(self):
-        response = requests.get(f"{self.base_url}/character/1000")
-        print(response.json())
+    def test_character_endpoint_should_return_list_when_some_params_empty(self):
+        response = requests.get(f"{self.base_url}/character/1,2,3,4,5,,7,,")
+        self.assertEqual(response.status_code, 200)
+        response_body = response.json()
+        self.assertGreater(len(response_body), 0)
 
-        response = requests.get(f"{self.base_url}/character/0")
-        print(response.json())
-
+    # NEGATIVE character endpoint tests
+    def test_character_should_return_error_for_an_invalid_param(self):
         response = requests.get(f"{self.base_url}/character/a")
-        print(response.json())
+        response_body = response.json()
+        self.assertEqual('error' in response_body, True)
+        self.assertEqual(response.status_code, 500)
 
+    def test_character_endpoint_returns_not_found_error_for_character_out_of_range(self):
+        response = requests.get(f"{self.base_url}/character/1000000")
+        response_body = response.json()
+        self.assertEqual('error' in response_body, True)
+        self.assertEqual(response.status_code, 404)
+        response = requests.get(f"{self.base_url}/character/0")
+        response_body = response.json()
+        self.assertEqual('error' in response_body, True)
+        self.assertEqual(response.status_code, 404)
+
+    def test_character(self):
         response = requests.get(f"{self.base_url}/character/1a")
         print(response.json())
 

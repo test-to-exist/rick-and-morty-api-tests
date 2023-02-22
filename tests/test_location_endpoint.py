be5018e27798b5fc6e99fb563773f1f70dbd22b5
Test To Exist <test-to-exist@proton.me>
2023-02-22 10:33:57 +0100

    Move test classes to a separate files

---
+++ tests/test_location_endpoint.py
@@ -0,0 +1,53 @@
+import unittest
+
+import requests
+
+
+class TestLocationEndpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.base_url = 'https://rickandmortyapi.com/api'
+
+    def test_location(self):
+        response = requests.get(f"{self.base_url}/location")
+        self.assertEqual(response.status_code, 200)
+        response_body = response.json()
+        self.assertGreater(len(response_body['results']), 0)
+
+        # response = requests.get(f"{self.base_url}/character/1")
+        # print(response.json())
+        #
+        # response = requests.get(f"{self.base_url}/character/1000")
+        # print(response.json())
+        #
+        # response = requests.get(f"{self.base_url}/character/0")
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/0")
+        print(0)
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/1")
+        print(1)
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/7")
+        print(7)
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/10000")
+        print(10000)
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/a")
+        print('a')
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/-3")
+        print('-3')
+        print(response.json())
+
+        response = requests.get(f"{self.base_url}/location/3")
+        print('3')
+        print(response.json())
+        # response = requests.get(f"{self.base_url}/character/a")

55bae839a0929032e875b3714841d3a0c78309ef
tomaszpichlak <tomasz@techtailor.co>
2023-09-17 09:23:53 +0200

    Remove parameterized and use subTest
    To have parameterized test

--- src/test/character/test_positive_character_endpoint.py
+++ src/test/character/test_positive_character_endpoint.py
@@ -6,7 +6,11 @@ from jsonschema import validate
 
 from src.config.config_to_env import load_config
 from src.schemas.character_schema import character_schema
-from parameterized import parameterized
+
+filter_param_list = [
+    ('name', 'Rick'),
+    ('status', 'Alive'),
+]
 
 
 class TestPositiveCharacterEndpoint(unittest.TestCase):
@@ -41,7 +45,7 @@ class TestPositiveCharacterEndpoint(unittest.TestCase):
         response_body = response.json()
         self.assertGreater(len(response_body), 0)
 
-# This works but remove after using parametrized
+    # This works but remove after using parametrized
     def test_character_list_should_be_filtered_by_name(self):
         name_param = 'Rick'
         response = requests.get(f"{self.base_url}/character?name={name_param}")
@@ -51,18 +55,12 @@ class TestPositiveCharacterEndpoint(unittest.TestCase):
         for name in map(lambda x: x['name'], response_body['results']):
             self.assertTrue(name_param in name)
 
-    # TODO - fix maybe we could use subTest solution
-    #  https://stackoverflow.com/questions/32899/how-do-you-generate-dynamic-parameterized-unit-tests-in-python
-    #
-    @parameterized.expand([
-        ('name', 'Rick'),
-        ('status', 'Alive'),
-    ])
-    def test_character_list_should_be_filtered_by_alive_status(self, filter_name, filter_val):
-        # filter_status = 'Alive'
-        response = requests.get(f"{self.base_url}/character?{filter_name}={filter_val}")
-        self.assertEqual(response.status_code, 200)
-        response_body = response.json()
-        self.assertTrue(len(response_body['results']) > 0)
-        for value in map(lambda x: x[filter_name], response_body['results']):
-            self.assertTrue(filter_val in value)
+    def test_character_list_should_be_filtered_by(self):
+        for filter_name, filter_val in filter_param_list:
+            with self.subTest(filter_name=filter_name, filter_val=filter_val):
+                response = requests.get(f"{self.base_url}/character?{filter_name}={filter_val}")
+                self.assertEqual(response.status_code, 200)
+                response_body = response.json()
+                self.assertTrue(len(response_body['results']) > 0)
+                for value in map(lambda x: x[filter_name], response_body['results']):
+                    self.assertTrue(filter_val in value)

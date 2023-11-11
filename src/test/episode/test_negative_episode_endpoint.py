df828a69f14c7fbdfa33a11cdb9af1586e671c59
tomaszpichlak <tomasz@techtailor.co>
2023-11-11 13:42:45 +0100

    Add negative tests

--- src/test/episode/test_negative_episode_endpoint.py
+++ src/test/episode/test_negative_episode_endpoint.py
@@ -6,22 +6,19 @@ import requests
 from src.config.config_to_env import load_config
 
 
-class TestNegativeCharacterEndpoint(unittest.TestCase):
+class TestNegativeEpisodeEndpoint(unittest.TestCase):
     def setUp(self):
         load_config()
         self.base_url = os.environ.get('BASEURL')
-        self.endpoint = 'character'
+        self.endpoint = 'episode'
 
-    def test_character_should_return_error_for_an_invalid_param(self):
-        """
-        Character endpoint should return an error for an invalid param
-        """
+    def test_episode_should_return_error_for_an_invalid_param(self):
         response = requests.get(f"{self.base_url}/{self.endpoint}/a")
         response_body = response.json()
         self.assertEqual('error' in response_body, True)
         self.assertEqual(response.status_code, 500)
 
-    def test_character_endpoint_returns_not_found_error_for_character_out_of_range(self):
+    def test_episode_endpoint_returns_not_found_error_for_episode_out_of_range(self):
         response = requests.get(f"{self.base_url}/{self.endpoint}/1000000")
         response_body = response.json()
         self.assertEqual('error' in response_body, True)
